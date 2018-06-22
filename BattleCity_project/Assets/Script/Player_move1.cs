3da6cf21570e1bfb65d127e59e57e673e3c2050f
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-06-22 16:31:24 +0900

    自機作成中まだ動かない。scriptにエラーはなし

---
+++ BattleCity_project/Assets/Script/Player_move1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move1 : MonoBehaviour {
+
+
+  public Vector2 SPEED = new Vector2(0.05f, 0.05f);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    Player_Move();
+	}
+  /*
+   * -------------------------
+   Player移動関数* 
+   ---------------------------*/
+  void Player_Move()
+  {
+    Vector2 Position = transform.position;
+      if(Input.GetKey("left"))
+    {
+      Position.x -= SPEED.x;
+    }
+    
+  }
+   
+}
