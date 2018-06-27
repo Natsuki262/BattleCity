414e3116c17164ba6e88d28d365e8d399b29f66b
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-06-27 17:20:30 +0900

    プレイヤーの方向転換制御に成功 弾はまだこれから

--- BattleCity_project/Assets/Script/Player_move1.cs
+++ BattleCity_project/Assets/Script/Player_move1.cs
@@ -2,77 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_move1 : MonoBehaviour {
+public class Player_move1 : MonoBehaviour
+{
 
 
-  public Vector2 SPEED = new Vector2(0.05f, 0.05f);
+    public Vector2 SPEED = new Vector2(0.05f, 0.05f);
     Vector2 Position;
-    
+    public GameObject P_Bullet;
+
+
     // Use this for initialization
-    void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Start()
     {
 
-    Player_Move();
-    }
-  /*
-   * -------------------------
-   Player移動関数* 
-   ---------------------------*/
-
-  void Player_Move()
-  {
-    Position = transform.position;
-      if(Input.GetKey(KeyCode.A))
-        {
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                transform.Rotate(new Vector3(0, 0,-90));
-            }
-            Position.x -= SPEED.x;
-            transform.position = Position;
-    }
-        transform.position = Position;
-
-        if(Input.GetKey(KeyCode.D))
-        {
-            //Dキーが押されたら右に動く
-            Position.x += SPEED.x;  
-          transform.position = Position;
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                transform.Rotate(new Vector3(0, 0, 90));
-            }
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            Position.y += SPEED.y;
-            transform.position = Position;
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                transform.Rotate(new Vector3(0, 0,-180));
-            }
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            Position.y -= SPEED.y;
-            transform.position = Position;
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                transform.Rotate(new Vector3(0, 0, 180));
-            }
-
-        }
-        
-
-
     }
 
-   
-}
+    // Update is called once per frame
+    void Update()
+    {
+
+        Player_Move();
+        BulletShot();
+
+    }    /*
+         * -------------------------
+         Player移動関数* 
+         ---------------------------*/
+
+        void Player_Move()
+        {
+            Position = transform.position;
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                //transform.Rotate(new Vector3(0, 0, -90));
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+                }
+                Position.x -= SPEED.x;
+                transform.position = Position;
+            }
+            transform.position = Position;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                //Dキーが押されたら右に動く
+                Position.x += SPEED.x;
+                transform.position = Position;
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                   //transform.Rotate(new Vector3(0, 0, 90));
+                   this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270.0f);
+                }
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                Position.y += SPEED.y;
+                transform.position = Position;
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                //transform.Rotate(new Vector3(0, 0, -180));
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            }
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                Position.y -= SPEED.y;
+                transform.position = Position;
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                // transform.Rotate(new Vector3(0, 0, 180));
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+                }
+
+            }
+        }
+        void BulletShot()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(P_Bullet, transform.position, Quaternion.identity);
+        }
+    }
+
+
+
+
+ } 
