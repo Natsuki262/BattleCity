1015d3546d3800eacd75c5b75423b5996db3c67e
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-07-05 17:55:37 +0900

    Player修正中

--- BattleCity_project/Assets/Script/Bullet.cs
+++ BattleCity_project/Assets/Script/Bullet.cs
@@ -5,37 +5,49 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 
 {
-    public GameObject bullet;
+   
 
-    public Transform Instance;
-
-
-    public float speed = 1000;
+    public float speed = 5;
+    public GameObject playerObject;
+    Vector3 playerRotation;
 
     // Use this for initialization
     void Start ()
     {
-       
-        
-           
-           
+        playerRotation = playerObject.transform.localEulerAngles;
+      if(playerRotation.z==0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5.0f);
+
+        }
+       else if (playerRotation.z == -90)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(5.0f, 0.0f);
+
+
+        }
+       else if (playerRotation.z == 180)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -5.0f);
+
+        }
+        else if (playerRotation.z == 90)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-5.0f, 0.0f);
+
+
+        }
+
+
+
+
+
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            // 弾丸の複製
-            GameObject bullets = GameObject.Instantiate(bullet) as GameObject;
-            Vector3 force;
-            force = this.gameObject.transform.forward * speed;
-            // Rigidbodyに力を加えて発射
-            bullets.GetComponent<Rigidbody>().AddForce(force);
-            // 弾丸の位置を調整
-            bullets.transform.position = Instance.position;
-
-        }
+        
     }
     
 }
--- BattleCity_project/Assets/Script/Player_move1.cs
+++ BattleCity_project/Assets/Script/Player_move1.cs
@@ -5,51 +5,141 @@ using UnityEngine;
 public class Player_move1 : MonoBehaviour
 {
 
-    GameObject Player;
-    GameObject Instance;
 
-    public GameObject Bullet;
-    public float speed;
+    public Vector2 SPEED = new Vector2(0.05f, 0.05f);
+    Vector2 Position;
+    public GameObject bullet;
+    public int X_Dintdirection;
+    public int Y_Dintdirection;
+    public float Z_Dintdirection;
+    public int speed = 10;
+    public int testnum = 100;
+    public int PlayerVector;
+    //定数の宣言
+    public const int VECTOR_UP = 1;
+    public const int VECTOR_DOWN = 2;
+    public const int VECTOR_RIGHT = 3;
+    public const int VECTOR_LEFT = 4;
+
     // Use this for initialization
     void Start()
     {
-        Player = GameObject.Find("Player");
-        Instance = GameObject.Find("Instance");
+       
+        
+            Instantiate (bullet, transform.position, transform.rotation);
+			// 0.05秒待つ
+			
 
+        
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            speed = 1000;
-           // Player.GetComponent().velocity = (Vector2.right * speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            speed = 1000;
-           // Player.GetComponent().velocity = (Vector2.right * -speed * Time.deltaTime);
-        }
-        else
-        {
-            speed = 0;
-            //Player.GetComponent().velocity = (Vector2.right * -speed * Time.deltaTime);
+            Instantiate(bullet, transform.position, transform.rotation);
         }
 
+        Player_Move();
 
 
+    }    /*
+         * -------------------------
+         Player移動関数* 
+         ---------------------------*/
+
+    void Player_Move()
+    {
+        Position = transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+                Z_Dintdirection = transform.eulerAngles.z;
+                Debug.Log(Z_Dintdirection);
+                PlayerVector = VECTOR_LEFT;
+            }
+            Position.x -= SPEED.x;
+            transform.position = Position;
+        }
+        transform.position = Position;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //Dキーが押されたら右に動く
+            Position.x += SPEED.x;
+            transform.position = Position;
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270.0f);
+
+                Z_Dintdirection = transform.eulerAngles.z;
+                Debug.Log(Z_Dintdirection);
+            }
+        }
+        //Wキー押されたら上に動く
+        if (Input.GetKey(KeyCode.W))
+        {
+            Position.y += SPEED.y;
+            transform.position = Position;
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+
+                Z_Dintdirection = transform.eulerAngles.z;
+                Debug.Log(Z_Dintdirection);
+
+            }
+        }
+
+        //Sキーを押されたら下に動く
+        if (Input.GetKey(KeyCode.S))
+        {
+            Position.y -= SPEED.y;
+            transform.position = Position;
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+
+                Z_Dintdirection = transform.eulerAngles.z;
+                Debug.Log(Z_Dintdirection);
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+            }
+
+        }
+    }
+    void Bulletgene()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            //GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+
+        }
 
     }
-
 
 }
 
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
 
 
 
