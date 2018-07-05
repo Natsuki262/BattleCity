d2055da537f856ef34f5b9412cc082264efbc391
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-07-05 14:29:57 +0900

    うんこ

---
+++ BattleCity_project/Assets/Script/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+
+{
+    public GameObject bullet;
+
+    public Transform Instance;
+
+
+    public float speed = 1000;
+
+    // Use this for initialization
+    void Start ()
+    {
+       
+        
+           
+           
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            // 弾丸の複製
+            GameObject bullets = GameObject.Instantiate(bullet) as GameObject;
+            Vector3 force;
+            force = this.gameObject.transform.forward * speed;
+            // Rigidbodyに力を加えて発射
+            bullets.GetComponent<Rigidbody>().AddForce(force);
+            // 弾丸の位置を調整
+            bullets.transform.position = Instance.position;
+
+        }
+    }
+    
+}
+
+
--- BattleCity_project/Assets/Script/Move.cs
+++ BattleCity_project/Assets/Script/Move.cs
@@ -2,26 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_move1 : MonoBehaviour
-{
-
+public class Move : MonoBehaviour {
     GameObject Player;
     GameObject Instance;
-
     public GameObject Bullet;
     public float speed;
     // Use this for initialization
-    void Start()
-    {
+    void Start ()
+    { 
         Player = GameObject.Find("Player");
         Instance = GameObject.Find("Instance");
-
-    }
-
-    // Update is called once per frame
-    void Update()
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
-
+        //移動処理
         if (Input.GetKey(KeyCode.RightArrow))
         {
             speed = 1000;
@@ -30,26 +27,12 @@ public class Player_move1 : MonoBehaviour
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             speed = 1000;
-           // Player.GetComponent().velocity = (Vector2.right * -speed * Time.deltaTime);
+            ///Player.GetComponent().velocity = (Vector2.right * -speed * Time.deltaTime);
         }
         else
         {
             speed = 0;
             //Player.GetComponent().velocity = (Vector2.right * -speed * Time.deltaTime);
         }
-
-
-
-
     }
-
-
 }
-
-
-
-
- 
-
-
-
